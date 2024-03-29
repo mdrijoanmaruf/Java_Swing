ae542f8d290f0692fca82ee1be544f367b0021c9
Md Rijoan Maruf <rmsaif4430@gmail.com>
2024-03-29 11:30:59 -0700

    Creating file

--- Test.java
+++ Test.java
@@ -1,6 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Test extends JFrame {
+    Test(){
+        Container c = this.getContentPane();
+        c.setLayout(null);
+
+        JLabel titleLable = new JLabel("This is the Title");
+        titleLable.setBounds(10,10,300,100);
+        c.add(titleLable);
+    }
     public static void main(String[] args) {
         Test t1 = new Test();
         t1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
