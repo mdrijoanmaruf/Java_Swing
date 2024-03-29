a7d68d93d2ed2b4860addffa5b695fe28a9b075c
Md Rijoan Maruf <rmsaif4430@gmail.com>
2024-03-29 11:26:37 -0700

    Creating file

---
+++ Test.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Test extends JFrame {
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        t1.setBounds(100,100,600,500);
+        t1.setVisible(true);
+    }
+}
